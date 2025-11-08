78706e69ed47d9b4ed8dfe3ebd924efc28c58142
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-09 00:07:55 +0600

    added the reveiw

--- src/controllers/cartCOntroller.js
+++ src/controllers/cartCOntroller.js
@@ -1,25 +1,25 @@
-// const { ObjectId } = require("mongodb");
-// const client = require("../helpers/client");
+const { ObjectId } = require("mongodb");
+const client = require("../helpers/client");
 
-// const cartsCollection = client.db("bistroRestaurant").collection("carts");
+const cartsCollection = client.db("bistroRestaurant").collection("carts");
 
-// // Add item to cart
-// const createCarts = async (req, res) => {
-//   const { name, image, recipe, price, userName, userEmail } = req.body;
+// Add item to cart
+const createCarts = async (req, res) => {
+  const { name, image, recipe, price, userName, userEmail } = req.body;
 
-//   if (!name || !image || !recipe || !price || !userEmail)
-//     return res.status(400).send({ success: false, message: "Missing fields!" });
+  if (!name || !image || !recipe || !price || !userEmail)
+    return res.status(400).send({ success: false, message: "Missing fields!" });
 
-//   const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
-//   const result = await cartsCollection.insertOne(newCart);
-//   res.send(result);
-// };
+  const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
+  const result = await cartsCollection.insertOne(newCart);
+  res.send(result);
+};
 
-// // Get user's cart
-// const getCarts = async (req, res) => {
-//   const query = req.query.email ? { userEmail: req.query.email } : {};
-//   const result = await cartsCollection.find(query).toArray();
-//   res.send(result);
-// };
+// Get user's cart
+const getCarts = async (req, res) => {
+  const query = req.query.email ? { userEmail: req.query.email } : {};
+  const result = await cartsCollection.find(query).toArray();
+  res.send(result);
+};
 
-// module.exports = { createCarts, getCarts };
+module.exports = { createCarts, getCarts };
