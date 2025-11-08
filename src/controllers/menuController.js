bbd016a7d6e04348bee5cb55e917f62f3020210b
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-09 00:07:31 +0600

    added the menu

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -1,18 +1,18 @@
-// const { ObjectId } = require("mongodb");
-// const client = require("../helpers/client");
+const { ObjectId } = require("mongodb");
+const client = require("../helpers/client");
 
-// const menuItemsCollection = client.db("bistroRestaurant").collection("menueITems");
-
-
-// // 1. Get All MenuItems
-// const getMenuItems = async (req, res) => {
-//   const MenuItems = await menuItemsCollection.find().toArray();
-//   res.status(200).json({ success: true, message: "All MenuItems", data: MenuItems });
-// };
+const menuItemsCollection = client.db("bistroRestaurant").collection("menueITems");
 
 
+// 1. Get All MenuItems
+const getMenuItems = async (req, res) => {
+  const MenuItems = await menuItemsCollection.find().toArray();
+  res.status(200).json({ success: true, message: "All MenuItems", data: MenuItems });
+};
 
-// module.exports = {
-//     getMenuItems,
 
-// };
+
+module.exports = {
+    getMenuItems,
+
+};
--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -1,10 +1,10 @@
-// const express = require("express");
-// const verifyToken = require("../midilewares/verifyToken");
-// const { createCarts, getCarts } = require("../controllers/cartCOntroller");
+const express = require("express");
+const verifyToken = require("../midilewares/verifyToken");
+const { createCarts, getCarts } = require("../controllers/cartCOntroller");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/", createCarts);
-// router.get("/", getCarts);
+router.post("/", createCarts);
+router.get("/", getCarts);
 
-// module.exports = router;
+module.exports = router;
