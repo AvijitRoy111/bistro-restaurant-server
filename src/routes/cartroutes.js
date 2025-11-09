28657cc641b2fd687115e65d4f33b1f2bfd6b8f8
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-10 03:00:13 +0600

    added the get

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -5,8 +5,8 @@ const { createCarts, getCarts, updateCartStatus, getAllOrders } = require("../co
 const router = express.Router();
 
 router.post("/", createCarts);
-// router.get("/", getCarts);
-// router.patch("/:id", updateCartStatus);
+router.get("/", getCarts);
+router.patch("/:id", updateCartStatus);
 // router.get("/orders", getAllOrders);
 
 // module.exports = router;
