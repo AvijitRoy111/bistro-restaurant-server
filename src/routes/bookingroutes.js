f07daf696081279bd2b34fca399ee64a8c187afe
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-10 03:02:46 +0600

    added the update booking

--- src/routes/bookingroutes.js
+++ src/routes/bookingroutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const verifyToken = require("../midilewares/verifyToken");
 const { createbookings, getbookings, updatebooking } = require("../controllers/bookingController");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get("/", getbookings);
-// router.post("/", createbookings);
-// router.patch("/:id", updatebooking)
+router.get("/", getbookings);
+router.post("/", createbookings);
+router.patch("/:id", updatebooking)
 
-// module.exports = router;
+module.exports = router;
