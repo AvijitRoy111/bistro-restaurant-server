b5440b4257f4567f257f77f676d9a15fbb59f3bc
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-10 02:59:49 +0600

    added the post

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const verifyToken = require("../midilewares/verifyToken");
 const { createCarts, getCarts, updateCartStatus, getAllOrders } = require("../controllers/cartCOntroller");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/", createCarts);
+router.post("/", createCarts);
 // router.get("/", getCarts);
 // router.patch("/:id", updateCartStatus);
 // router.get("/orders", getAllOrders);
