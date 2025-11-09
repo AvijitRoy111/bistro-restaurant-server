b5398482eb56760c1d68ede6ae867a953ab85d81
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-10 02:57:08 +0600

    added the user cart

--- src/controllers/cartCOntroller.js
+++ src/controllers/cartCOntroller.js
@@ -1,19 +1,19 @@
-// const { ObjectId } = require("mongodb");
-// const client = require("../helpers/client");
+const { ObjectId } = require("mongodb");
+const client = require("../helpers/client");
 
-// const cartsCollection = client.db("bistroRestaurant").collection("carts");
+const cartsCollection = client.db("bistroRestaurant").collection("carts");
 
-// // Add item to cart
-// const createCarts = async (req, res) => {
-//   const { name, image, recipe, price, userName, userEmail } = req.body;
+// Add item to cart
+const createCarts = async (req, res) => {
+  const { name, image, recipe, price, userName, userEmail } = req.body;
 
-//   if (!name || !image || !recipe || !price || !userEmail)
-//     return res.status(400).send({ success: false, message: "Missing fields!" });
+  if (!name || !image || !recipe || !price || !userEmail)
+    return res.status(400).send({ success: false, message: "Missing fields!" });
 
-//   const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
-//   const result = await cartsCollection.insertOne(newCart);
-//   res.send(result);
-// };
+  const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
+  const result = await cartsCollection.insertOne(newCart);
+  res.send(result);
+};
 
 // // Get user's cart
 // const getCarts = async (req, res) => {
