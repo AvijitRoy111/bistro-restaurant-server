d226fd796c15b5a19beea84dbeae358ff5923cf3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-11 02:17:42 +0600

    added the express router

--- src/routes/contactroutes.js
+++ src/routes/contactroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { addedContact } = require("../controllers/contactController");
 
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.post("/", addedContact);
 
