115311e80de1c0b8de7a82b367cea5e200a15221
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-20 21:27:19 +0600

    added the get one

--- src/controllers/reveiwController.js
+++ src/controllers/reveiwController.js
@@ -3,12 +3,12 @@
 // const { getCollection } = require("../helpers/mongo");
 
 
-// // 1. Get All reveiws
-// const getreveiws = async (req, res) => {
-//   const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
-//   const reveiws = await reviewsCollection.find().toArray();
-//   res.status(200).json({ success: true, message: "All reveiws", data: reveiws });
-// };
+// 1. Get All reveiws
+const getreveiws = async (req, res) => {
+  const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
+  const reveiws = await reviewsCollection.find().toArray();
+  res.status(200).json({ success: true, message: "All reveiws", data: reveiws });
+};
 
 // 2. Create reveiw
 const createreveiws = async (req, res) => {
