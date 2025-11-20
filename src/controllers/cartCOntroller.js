e62f06b39fc8e151f4837bc1e7823a841ee6a761
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-20 21:19:42 +0600

    added the change

--- src/controllers/cartCOntroller.js
+++ src/controllers/cartCOntroller.js
@@ -1,64 +1,64 @@
-const { ObjectId } = require("mongodb");
-const { getCollection } = require("../helpers/mongo");
+// const { ObjectId } = require("mongodb");
+// const { getCollection } = require("../helpers/mongo");
 
-// Add item to cart
-const createCarts = async (req, res) => {
-  const { name, image, recipe, price, userName, userEmail } = req.body;
+// // Add item to cart
+// const createCarts = async (req, res) => {
+//   const { name, image, recipe, price, userName, userEmail } = req.body;
 
-  if (!name || !image || !recipe || !price || !userEmail)
-    return res.status(400).send({ success: false, message: "Missing fields!" });
+//   if (!name || !image || !recipe || !price || !userEmail)
+//     return res.status(400).send({ success: false, message: "Missing fields!" });
 
-  const cartsCollection = await getCollection("bistroRestaurant", "carts");
-  const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
-  const result = await cartsCollection.insertOne(newCart);
-  res.send(result);
-};
+//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+//   const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
+//   const result = await cartsCollection.insertOne(newCart);
+//   res.send(result);
+// };
 
-// Get user's cart
-const getCarts = async (req, res) => {
-  const query = req.query.email ? { userEmail: req.query.email } : {};
-  const cartsCollection = await getCollection("bistroRestaurant", "carts");
-  const result = await cartsCollection.find(query).toArray();
-  res.send(result);
-};
+// // Get user's cart
+// const getCarts = async (req, res) => {
+//   const query = req.query.email ? { userEmail: req.query.email } : {};
+//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+//   const result = await cartsCollection.find(query).toArray();
+//   res.send(result);
+// };
 
-// Update order status
-const updateCartStatus = async (req, res) => {
-  const { id } = req.params;
-  const { status } = req.body;
+// // Update order status
+// const updateCartStatus = async (req, res) => {
+//   const { id } = req.params;
+//   const { status } = req.body;
 
-  const cartsCollection = await getCollection("bistroRestaurant", "carts");
-  const result = await cartsCollection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: { status } }
-  );
+//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+//   const result = await cartsCollection.updateOne(
+//     { _id: new ObjectId(id) },
+//     { $set: { status } }
+//   );
 
-  res.send({ success: true, data: result });
-};
+//   res.send({ success: true, data: result });
+// };
 
-// Get all or filtered orders (for ManageOrder)
-const getAllOrders = async (req, res) => {
-  const status = req.query.status;
-  const query = status ? { status } : {};
-  const cartsCollection = await getCollection("bistroRestaurant", "carts");
-  const result = await cartsCollection.find(query).toArray();
-  res.send(result);
-};
+// // Get all or filtered orders (for ManageOrder)
+// const getAllOrders = async (req, res) => {
+//   const status = req.query.status;
+//   const query = status ? { status } : {};
+//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+//   const result = await cartsCollection.find(query).toArray();
+//   res.send(result);
+// };
 
 
-// Delete order
-const deleteOrder = async (req, res) => {
-  const id = req.params.id;
-  const cartsCollection = await getCollection("bistroRestaurant", "carts");
-  const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
-  res.status(200).json({ success: true, message: "order deleted", data: result });
-};
+// // Delete order
+// const deleteOrder = async (req, res) => {
+//   const id = req.params.id;
+//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+//   const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
+//   res.status(200).json({ success: true, message: "order deleted", data: result });
+// };
 
-module.exports =
-{
-  createCarts,
-  getCarts,
-  updateCartStatus,
-  getAllOrders,
-  deleteOrder
-};
+// module.exports =
+// {
+//   createCarts,
+//   getCarts,
+//   updateCartStatus,
+//   getAllOrders,
+//   deleteOrder
+// };
