133aa98c80f22f3cd4a482753fd5e700cbf496de
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-20 21:26:26 +0600

    added the change

--- src/controllers/reveiwController.js
+++ src/controllers/reveiwController.js
@@ -1,26 +1,26 @@
-// src/controllers/reviewController.js
-const { ObjectId } = require("mongodb");
-const { getCollection } = require("../helpers/mongo");
+// // src/controllers/reviewController.js
+// const { ObjectId } = require("mongodb");
+// const { getCollection } = require("../helpers/mongo");
 
 
-// 1. Get All reveiws
-const getreveiws = async (req, res) => {
-  const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
-  const reveiws = await reviewsCollection.find().toArray();
-  res.status(200).json({ success: true, message: "All reveiws", data: reveiws });
-};
+// // 1. Get All reveiws
+// const getreveiws = async (req, res) => {
+//   const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
+//   const reveiws = await reviewsCollection.find().toArray();
+//   res.status(200).json({ success: true, message: "All reveiws", data: reveiws });
+// };
 
-// 2. Create reveiw
-const createreveiws = async (req, res) => {
-  const reveiwData = req.body;
-  const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
-  const result = await reviewsCollection.insertOne(reveiwData);
-  res.status(200).json({ success: true, message: "reveiws created", data: result });
-};
+// // 2. Create reveiw
+// const createreveiws = async (req, res) => {
+//   const reveiwData = req.body;
+//   const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
+//   const result = await reviewsCollection.insertOne(reveiwData);
+//   res.status(200).json({ success: true, message: "reveiws created", data: result });
+// };
 
 
 
-module.exports = {
-    getreveiws,
-    createreveiws
-};
+// module.exports = {
+//     getreveiws,
+//     createreveiws
+// };
