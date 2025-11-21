ebf102063aad827fa4f81a68ee6a518e5758046b
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-21 21:59:25 +0600

    added the addded

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,13 +1,13 @@
 // const { ObjectId } = require("mongodb");
 // const { getCollection } = require("../helpers/mongo");
 
-// // create contact
-// const addedContact = async (req, res) => {
-//   const ContactData = req.body;
-//   const contactCollection = await getCollection("bistroRestaurant", "contacts");
-//   const result = await contactCollection.insertOne(ContactData);
-//   res.status(201).json({ success: true, message: "Contact created", data: result });
-// }
+// create contact
+const addedContact = async (req, res) => {
+  const ContactData = req.body;
+  const contactCollection = await getCollection("bistroRestaurant", "contacts");
+  const result = await contactCollection.insertOne(ContactData);
+  res.status(201).json({ success: true, message: "Contact created", data: result });
+}
 
 
 const getAllContact = async (req, res) => {
