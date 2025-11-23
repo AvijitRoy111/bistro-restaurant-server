99a15a3d6f77786899b04d2c24c2ac1d224d5dc1
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-23 18:28:56 +0600

    added the update

--- src/helpers/mongo.js
+++ src/helpers/mongo.js
@@ -1,19 +1,19 @@
-const client = require("./client");
+// const client = require("./client");
 
-async function ensureConnected() {
-  if (!client) throw new Error("MONGODB_URI is not configured");
-  // The MongoClient instance has its own connect() method.
-  if (!client.topLevelConnected) {
-    await client.connect();
-    client.topLevelConnected = true;
-  }
-}
+// async function ensureConnected() {
+//   if (!client) throw new Error("MONGODB_URI is not configured");
+//   // The MongoClient instance has its own connect() method.
+//   if (!client.topLevelConnected) {
+//     await client.connect();
+//     client.topLevelConnected = true;
+//   }
+// }
 
-async function getCollection(dbName, collName) {
-  await ensureConnected();
-  return client.db(dbName).collection(collName);
-}
+// async function getCollection(dbName, collName) {
+//   await ensureConnected();
+//   return client.db(dbName).collection(collName);
+// }
 
-module.exports = {
-  getCollection,
-};
+// module.exports = {
+//   getCollection,
+// };
