4b9e83bf293173a369fc11b65aed0f2a321ac679
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-23 18:30:41 +0600

    added the fuction

--- src/helpers/mongo.js
+++ src/helpers/mongo.js
@@ -1,8 +1,8 @@
 // const client = require("./client");
 
-// async function ensureConnected() {
-//   if (!client) throw new Error("MONGODB_URI is not configured");
-//   // The MongoClient instance has its own connect() method.
+async function ensureConnected() {
+  if (!client) throw new Error("MONGODB_URI is not configured");
+  // The MongoClient instance has its own connect() method.
   if (!client.topLevelConnected) {
     await client.connect();
     client.topLevelConnected = true;
