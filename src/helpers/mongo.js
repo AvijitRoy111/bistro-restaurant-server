926596deeec065cfd0afb33d7803b4e872d3ca24
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-23 18:30:16 +0600

    added the change

--- src/helpers/mongo.js
+++ src/helpers/mongo.js
@@ -3,11 +3,11 @@
 // async function ensureConnected() {
 //   if (!client) throw new Error("MONGODB_URI is not configured");
 //   // The MongoClient instance has its own connect() method.
-//   if (!client.topLevelConnected) {
-//     await client.connect();
-//     client.topLevelConnected = true;
-//   }
-// }
+  if (!client.topLevelConnected) {
+    await client.connect();
+    client.topLevelConnected = true;
+  }
+}
 
 async function getCollection(dbName, collName) {
   await ensureConnected();
