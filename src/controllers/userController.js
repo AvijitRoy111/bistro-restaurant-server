8e8f4f3d2aead447111d9537806b281634b0d619
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-23 18:26:22 +0600

    ädded the gett all user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,12 +21,12 @@
 //   }
 // };
 
-// // 2 Get all users
-// const getAllUsers = async (req, res) => {
-//   const usersCollection = await getCollection("bistroRestaurant", "users");
-//   const result = await usersCollection.find().toArray();
-//   res.status(200).json({ success: true, message: "Get all users", data: result });
-// };
+// 2 Get all users
+const getAllUsers = async (req, res) => {
+  const usersCollection = await getCollection("bistroRestaurant", "users");
+  const result = await usersCollection.find().toArray();
+  res.status(200).json({ success: true, message: "Get all users", data: result });
+};
 
 // 3 Delete user
 const deleteUser = async (req, res) => {
