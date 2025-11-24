9564f4425d1b3c7d0e5b316a73736d6c066d5fa4
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-24 15:26:51 +0600

    added the const

--- src/midilewares/verifyToken.js
+++ src/midilewares/verifyToken.js
@@ -1,6 +1,6 @@
 // const jwt = require("jsonwebtoken");
 
-// const ACCESS_SECRET_TOKEN="ed3010263b385f044d1473b033012ef2eea71b9c5d47a1517b9bd1f37b9a2059a73c848d8b321e0e0cbb2e1a6434bd7d4475572e1d19d9835fc98741880c685d"
+const ACCESS_SECRET_TOKEN="ed3010263b385f044d1473b033012ef2eea71b9c5d47a1517b9bd1f37b9a2059a73c848d8b321e0e0cbb2e1a6434bd7d4475572e1d19d9835fc98741880c685d"
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
