9dac76b3dfa21c65581bda40016a306caa6a6555
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-24 15:27:43 +0600

    added the change

--- src/routes/bookingroutes.js
+++ src/routes/bookingroutes.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const {
-  createBooking,
-  getBookings,
-  updateBooking,
-  deleteBooking,
-} = require("../controllers/bookingController");
+// const express = require("express");
+// const {
+//   createBooking,
+//   getBookings,
+//   updateBooking,
+//   deleteBooking,
+// } = require("../controllers/bookingController");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", getBookings);
-router.post("/", createBooking);
-router.patch("/:id", updateBooking);
-router.delete("/:id", deleteBooking); 
+// router.get("/", getBookings);
+// router.post("/", createBooking);
+// router.patch("/:id", updateBooking);
+// router.delete("/:id", deleteBooking); 
 
-module.exports = router;
+// module.exports = router;
