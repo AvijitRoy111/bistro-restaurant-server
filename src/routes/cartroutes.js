a2b3385143456659c518c787377e09c8620fd695
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-24 15:34:28 +0600

    added the get all order

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -13,7 +13,7 @@
 // router.post("/", createCarts);
 // router.get("/", getCarts);
 // router.patch("/:id", updateCartStatus);
-// router.get("/orders", getAllOrders);
+router.get("/orders", getAllOrders);
 router.delete("/:id", deleteOrder)
 
 module.exports = router;
