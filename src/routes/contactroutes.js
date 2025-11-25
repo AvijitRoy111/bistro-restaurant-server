ef4d49bb8793c23b4cc1465ac5ccf82d0c01c0b1
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-25 20:28:55 +0600

    added the added contact

--- src/routes/contactroutes.js
+++ src/routes/contactroutes.js
@@ -4,7 +4,7 @@
 
 // const router = express.Router();
 
-// router.post("/", addedContact);
+router.post("/", addedContact);
 router.get("/", getAllContact);
 router.delete("/:id", deleteContact);
 
