fc1253a0abecb314ef89f6cacb1c90fb71d6ca1e
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-25 20:39:02 +0600

    added the jwt

--- src/routes/menuroutes.js
+++ src/routes/menuroutes.js
@@ -1,5 +1,5 @@
 // const express = require("express");
-// const verifyToken = require("../midilewares/verifyToken");
+const verifyToken = require("../midilewares/verifyToken");
 const { getMenuItems, createMenuItems, deleteMenuItem, updateMenuItem } = require("../controllers/menuController");
 
 const router = express.Router();
