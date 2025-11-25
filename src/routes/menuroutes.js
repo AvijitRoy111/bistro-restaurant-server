3465892cf40892f591502fbacd3775514b12472a
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-25 20:38:46 +0600

    added the const

--- src/routes/menuroutes.js
+++ src/routes/menuroutes.js
@@ -1,6 +1,6 @@
 // const express = require("express");
 // const verifyToken = require("../midilewares/verifyToken");
-// const { getMenuItems, createMenuItems, deleteMenuItem, updateMenuItem } = require("../controllers/menuController");
+const { getMenuItems, createMenuItems, deleteMenuItem, updateMenuItem } = require("../controllers/menuController");
 
 const router = express.Router();
 
