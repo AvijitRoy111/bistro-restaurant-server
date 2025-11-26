7e73f73bdfc455f924bd332fc763fc34382aaed3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-26 20:51:33 +0600

    added the update

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -1,9 +1,9 @@
-// const express = require("express");
-// const verifyToken = require("../midilewares/verifyToken");
-// const { createUsers, getAllUsers, deleteUser } = require("../controllers/userController");
+const express = require("express");
+const verifyToken = require("../midilewares/verifyToken");
+const { createUsers, getAllUsers, deleteUser } = require("../controllers/userController");
 
 
-// const router = express.Router();
+const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/", createUsers)
