d27a7906f607b6b1cfe35c683e3c0b176f1f68fb
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-26 20:50:34 +0600

    added the change

--- src/routes/userroutes.js
+++ src/routes/userroutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const verifyToken = require("../midilewares/verifyToken");
-const { createUsers, getAllUsers, deleteUser } = require("../controllers/userController");
+// const express = require("express");
+// const verifyToken = require("../midilewares/verifyToken");
+// const { createUsers, getAllUsers, deleteUser } = require("../controllers/userController");
 
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get("/", getAllUsers);
-router.post("/", createUsers)
-router.delete("/:id", deleteUser);
+// router.get("/", getAllUsers);
+// router.post("/", createUsers)
+// router.delete("/:id", deleteUser);
 
-module.exports = router;
+// module.exports = router;
