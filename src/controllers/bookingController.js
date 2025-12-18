58ff1d3a7d5e0fdf45917ddb4b7259a700dbcd81
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-18 15:07:18 +0600

    added the result

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -12,7 +12,7 @@ const getBookings = async (req, res) => {
 const createBooking = async (req, res) => {
   const bookingData = req.body;
   const bookingsCollection = await getCollection("bistroRestaurant", "bookings");
-//   const result = await bookingsCollection.insertOne(bookingData);
+  const result = await bookingsCollection.insertOne(bookingData);
 //   res.status(200).json({ success: true, message: "Booking created", data: result });
 // };
 
