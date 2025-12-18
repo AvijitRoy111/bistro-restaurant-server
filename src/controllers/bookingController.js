ffd7bb5d0475afba19d71aaea9b5b38e19f90e64
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-18 15:09:23 +0600

    added the update

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -18,7 +18,7 @@ const createBooking = async (req, res) => {
 
 //  Update Booking Status
 const updateBooking = async (req, res) => {
-//   const id = req.params.id;
+  const id = req.params.id;
 //   const { status } = req.body;
 
 //   const bookingsCollection = await getCollection("bistroRestaurant", "bookings");
