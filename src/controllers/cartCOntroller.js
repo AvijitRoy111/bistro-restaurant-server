86192d272c4edc8ea14f89f1b1d456da4ea3bfb3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-19 19:53:49 +0600

    added the result

--- src/controllers/cartCOntroller.js
+++ src/controllers/cartCOntroller.js
@@ -11,7 +11,7 @@ const createCarts = async (req, res) => {
   const cartsCollection = await getCollection("bistroRestaurant", "carts");
   const newCart = { name, image, recipe, price, userName, userEmail, addedAt: new Date() };
   const result = await cartsCollection.insertOne(newCart);
-//   res.send(result);
+  res.send(result);
 // };
 
 // // Get user's cart
