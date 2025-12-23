da5f9cd03b6c78b36454c2918aa9bd2748ed6368
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-23 19:00:05 +0600

    added the result

--- src/controllers/cartCOntroller.js
+++ src/controllers/cartCOntroller.js
@@ -50,7 +50,7 @@ const getAllOrders = async (req, res) => {
 const deleteOrder = async (req, res) => {
   const id = req.params.id;
   const cartsCollection = await getCollection("bistroRestaurant", "carts");
-//   const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
+  const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
 //   res.status(200).json({ success: true, message: "order deleted", data: result });
 // };
 
