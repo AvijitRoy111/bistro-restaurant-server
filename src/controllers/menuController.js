aab59ca320a2c83007c7be1fcabc330a3d45de54
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-25 21:01:05 +0600

    added the create data

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -12,7 +12,7 @@ const getMenuItems = async (req, res) => {
 // 2.add a menuItems
 const createMenuItems = async (req, res) =>{
   const menudata = req.body;
-//   const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
+  const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
 //   const result = await menuItemsCollection.insertOne(menudata);
 //   res.status(200).json({success:true, massage:'add menuItems', data:result})
 // }
