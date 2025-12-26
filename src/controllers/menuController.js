e9c9035916cfc93b4847bc08464674949edcd9c3
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-27 00:00:52 +0600

    added the update

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -60,7 +60,7 @@ const updateMenuItem = async (req, res) => {
     }
   } catch (error) {
     console.error("Update error:", error);
-//     res.status(500).json({ success: false, message: "Failed to update item", error });
+    res.status(500).json({ success: false, message: "Failed to update item", error });
 //   }
 // };
 
