4fe18b26b9bf17da6ce1e0d80ff19f9bf33e9105
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-27 00:05:49 +0600

    added the cart

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -9,7 +9,7 @@ const placeOrder = async (req, res) => {
     order.addedAt = new Date();
 
   const cartsCollection = await getCollection("bistroRestaurant", "carts");
-//   const result = await cartsCollection.insertOne(order);
+  const result = await cartsCollection.insertOne(order);
 //   res.status(201).json({ success: true, message: "Order placed", data: result });
 //   } catch (err) {
 //     res.status(500).json({ success: false, message: err.message });
