8fbeeef32e816d096584c87ae5503de6a44a4c48
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-26 23:56:46 +0600

    added the result

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -55,7 +55,7 @@ const updateMenuItem = async (req, res) => {
 
     if (result.modifiedCount > 0) {
       res.status(200).json({ success: true, message: "Menu item updated successfully" });
-//     } else {
+    } else {
 //       res.status(404).json({ success: false, message: "Item not found or no change" });
 //     }
 //   } catch (error) {
