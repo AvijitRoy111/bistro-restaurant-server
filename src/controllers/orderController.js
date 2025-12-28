828220d015df9449329b489e81b6f19b300e2e53
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-28 19:18:11 +0600

    added the result

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -21,7 +21,7 @@ const getPendingOrders = async (req, res) => {
   try {
   const cartsCollection = await getCollection("bistroRestaurant", "carts");
   const result = await cartsCollection.find({ status: "pending" }).toArray();
-//   res.status(200).json(result);
+  res.status(200).json(result);
 //   } catch (err) {
 //     res.status(500).json({ success: false, message: err.message });
 //   }
