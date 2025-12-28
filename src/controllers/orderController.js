7ed664a79e0b3d797bd327f64b0bcbeb0558402b
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-28 19:28:28 +0600

    added the update

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -52,7 +52,7 @@ const deleteOrder = async (req, res) => {
   const cartsCollection = await getCollection("bistroRestaurant", "carts");
   const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
   res.json({ success: true, message: "Order deleted", data: result });
-//   } catch (err) {
+  } catch (err) {
 //     res.status(500).json({ success: false, message: err.message });
 //   }
 // };
