0e39a403117063c6851514a5632a50505fde9c7d
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-28 19:27:37 +0600

    added the change

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -48,8 +48,8 @@ const updateOrderStatus = async (req, res) => {
 //  Delete Order
 const deleteOrder = async (req, res) => {
   try {
-//     const { id } = req.params;
-//   const cartsCollection = await getCollection("bistroRestaurant", "carts");
+    const { id } = req.params;
+  const cartsCollection = await getCollection("bistroRestaurant", "carts");
 //   const result = await cartsCollection.deleteOne({ _id: new ObjectId(id) });
 //   res.json({ success: true, message: "Order deleted", data: result });
 //   } catch (err) {
