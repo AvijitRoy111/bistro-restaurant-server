20f445cacd318d7833890eeee1948bf3c37b07e9
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-30 19:45:54 +0600

    added the delete contact

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -20,7 +20,7 @@ const deleteContact = async (req, res) => {
   const id = req.params.id;
   const contactCollection = await getCollection("bistroRestaurant", "contacts");
   const result = await contactCollection.deleteOne({ _id: new ObjectId(id) });
-//   res.status(200).json({ success: true, message: "Contact deleted", data: result });
+  res.status(200).json({ success: true, message: "Contact deleted", data: result });
 // };
 
 
