9b6947d5090eb96c4964ce4b8ea8db5db685e6b0
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-31 19:01:05 +0600

    added the delete menu

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -22,7 +22,7 @@ const deleteMenuItem = async (req, res) => {
   try {
     const id = req.params.id;
     let query = { _id: new ObjectId(id) };
-//     const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
+    const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
 //     let result = await menuItemsCollection.deleteOne(query);
 //     if (result.deletedCount === 0) {
 //       query = { _id: id };
