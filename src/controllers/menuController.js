0b1089c1648e3a0d2c69085b1e826e04a327a1ae
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-31 18:57:52 +0600

    added the get menu

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -3,8 +3,8 @@ const { getCollection } = require("../helpers/mongo");
 
 
 // 1. Get All MenuItems
-// const getMenuItems = async (req, res) => {
-//   const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
+const getMenuItems = async (req, res) => {
+  const menuItemsCollection = await getCollection("bistroRestaurant", "menueITems");
 //   const MenuItems = await menuItemsCollection.find().toArray();
 //   res.status(200).json({ success: true, message: "All MenuItems", data: MenuItems });
 // };
