cc88674ee7cecc3b6bcafa2a238a0b1eb6f65692
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-05 17:30:07 +0600

    added the reveiws

--- src/controllers/reveiwController.js
+++ src/controllers/reveiwController.js
@@ -4,7 +4,7 @@ const { getCollection } = require("../helpers/mongo");
 
 // 1. Get All reveiws
 const getreveiws = async (req, res) => {
-//   const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
+  const reviewsCollection = await getCollection("bistroRestaurant", "reveiws");
 //   const reveiws = await reviewsCollection.find().toArray();
 //   res.status(200).json({ success: true, message: "All reveiws", data: reveiws });
 // };
