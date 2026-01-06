d42f42f8578127740c0406537dfa5be6f2c18a4a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-06 19:25:59 +0600

    added the create user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,7 +17,7 @@ const createUsers = async (req, res) => {
   const result = await usersCollection.insertOne(userData);
     res.status(200).json({ success: true, message: "User added successfully", data: result });
   } catch (error) {
-//     res.status(500).json({ success: false, message: "Error creating user", error: error.message });
+    res.status(500).json({ success: false, message: "Error creating user", error: error.message });
 //   }
 // };
 
