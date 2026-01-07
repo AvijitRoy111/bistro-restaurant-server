fdf35f452a5041c080428eee129aeca9ed004c00
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-07 18:29:11 +0600

    added the change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,7 +37,7 @@ const deleteUser = async (req, res) => {
   res.status(200).json({ success: true, message: "User deleted", data: result });
 };
 
-// module.exports = {
+module.exports = {
 //   createUsers,
 //   getAllUsers,
 //   deleteUser,
