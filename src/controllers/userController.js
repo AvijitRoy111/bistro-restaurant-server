450d9e2c564b09752ba4eec47fdd47b101be0317
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-07 18:26:38 +0600

    added the delete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,7 +30,7 @@ const getAllUsers = async (req, res) => {
 
 // 3 Delete user
 const deleteUser = async (req, res) => {
-//   const id = req.params.id;
+  const id = req.params.id;
 //   const query = { _id: new ObjectId(id) };
 //   const usersCollection = await getCollection("bistroRestaurant", "users");
 //   const result = await usersCollection.deleteOne(query);
