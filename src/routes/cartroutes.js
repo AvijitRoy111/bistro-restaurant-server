e895287c3bd86467b4a03209cb72af75faa1690a
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-09 20:36:20 +0600

    added the update

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const verifyToken = require("../midilewares/verifyToken");
-const { 
-    createCarts, 
-    getCarts, 
-    updateCartStatus, 
-    getAllOrders, 
-    deleteOrder, 
-} = require("../controllers/cartCOntroller");
+// const express = require("express");
+// const verifyToken = require("../midilewares/verifyToken");
+// const { 
+//     createCarts, 
+//     getCarts, 
+//     updateCartStatus, 
+//     getAllOrders, 
+//     deleteOrder, 
+// } = require("../controllers/cartCOntroller");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/", createCarts);
-router.get("/", getCarts);
-router.patch("/:id", updateCartStatus);
-router.get("/orders", getAllOrders);
-router.delete("/:id", deleteOrder)
+// router.post("/", createCarts);
+// router.get("/", getCarts);
+// router.patch("/:id", updateCartStatus);
+// router.get("/orders", getAllOrders);
+// router.delete("/:id", deleteOrder)
 
-module.exports = router;
+// module.exports = router;
