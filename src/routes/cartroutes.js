e4f490d0beb8243514db4786f1aeab4aaf5f5e0f
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-09 20:37:36 +0600

    added the create  cart

--- src/routes/cartroutes.js
+++ src/routes/cartroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const verifyToken = require("../midilewares/verifyToken");
 const { 
     createCarts, 
-//     getCarts, 
+    getCarts, 
 //     updateCartStatus, 
 //     getAllOrders, 
 //     deleteOrder, 
