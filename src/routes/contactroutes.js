f70ae404363133dd483ba9343836cbee1cc75df3
AvijitRoy111 <bidhanbormon24@gmail.com>
2026-01-10 19:01:17 +0600

    added the change

--- src/routes/contactroutes.js
+++ src/routes/contactroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { addedContact, getAllContact, deleteContact } = require("../controllers/contactController");
 
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.post("/", addedContact);
 // router.get("/", getAllContact);
